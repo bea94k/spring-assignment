dd258ede94100fcc2c8da8f67594be0b8e803670
BeaK94 <beatakusnierz94@gmail.com>
2020-02-22 14:46:07 +0200

    Skill cards, contact - CV download button
    Added:
    - Fontawesome .js
    - icons and flags to skill cards
    - progress bars to skill cards
    - fonts
    - CV download button
    - button styling
    - minor changes to the footer.

--- beata/js/main.js
+++ beata/js/main.js
@@ -2,4 +2,9 @@ function sayHi() {
     console.log('henlo der')
 }
 
-sayHi()
+sayHi();
+
+
+$('.cv-btn').click(function() {
+    window.open("https://drive.google.com/file/d/1jB85BXs7fTBjAALDJ_OYJXcQTonPAX_I/view?usp=sharing")
+  });
