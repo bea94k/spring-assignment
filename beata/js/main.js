82270970745e640133f2a82f4ef87ea01bcabc3d
BeaK94 <beatakusnierz94@gmail.com>
2020-02-24 17:53:57 +0200

    Edu and exp cards filled, progress bars corrected, img slider kind of responsive
    - edu and exp cards formatted
    - edu cards filled with text
    - exp cards filled with placeholders
    - progress bars put on correct level
    - image slider responsive - stage 1, difference for big screens and others
    - minor changes to styling and comments in code

--- beata/js/main.js
+++ beata/js/main.js
@@ -1,17 +1,23 @@
-function sayHi() {
-  console.log("henlo der");
-}
-
-sayHi();
-
-
+/* CV button opens a pdf file with an option of downloading the file */
 $('.cv-btn').click(function() {
     window.open("https://drive.google.com/file/d/1jB85BXs7fTBjAALDJ_OYJXcQTonPAX_I/view?usp=sharing")
   });
 
+/* SLIDER - depending on the width of the user's screen when the page is loaded, the slider has different properties (doesn't work when resizing the window - only with resize + refresh page) */
+if ($(window).width() > "840") { /* when user's screen is wider than 840px, do this */
+  $(".slider").slick({
+    dots: true,
+    wariableWidth: true,
+    wariableHeight: true
+  });
+} else {
+  $(".slider").slick({
+    dots: true,
+    arrows: false,
+    wariableWidth: true,
+    wariableHeight: true
+  });
+}
 
-$(".slider").slick({
-  dots: true,
-  wariableWidth: true,
-  wariableHeight: true
-});
+
+
